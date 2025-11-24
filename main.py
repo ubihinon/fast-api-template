ded2072662b4f49da1fd7a69c622bd11a2fc105d
ubihinon <ubihinon1@gmail.com>
2025-11-24 23:55:30 +0400

    fix

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from typing import Annotated, List
 
 import sqlalchemy
 from fastapi import FastAPI
-from sqlalchemy import create_engine, select, String, text, UUID
+from pydantic import BaseModel, EmailStr
+from sqlalchemy import create_engine, func, Index, Integer, select, String, text, UUID
 from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession, create_async_engine
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, validates
 
 import settings
 
@@ -25,8 +26,8 @@ class Base(DeclarativeBase):
 
 app = FastAPI()
 
-
-UUIDPK = Annotated[uuid.UUID, mapped_column(UUID(as_uuid=True), primary_key=True)]
+UUIDPK = Annotated[UUID, mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)]
+IntPK = Annotated[int, mapped_column(primary_key=True, autoincrement=True)]
 CreatedAt = Annotated[
     datetime.datetime, mapped_column(server_default=text("(CURRENT_TIMESTAMP AT TIME ZONE 'UTC')"), nullable=False)
 ]
@@ -42,30 +43,57 @@ UpdatedAt = Annotated[
 class User(Base):
     __tablename__ = 'user'
 
-    id: Mapped[UUIDPK]
+    id: Mapped[IntPK]
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     is_active: Mapped[bool] = mapped_column(default=False)
     created_at: Mapped[CreatedAt]
     updated_at: Mapped[UpdatedAt]
 
+    __table_args__ = (
+        Index('ix_user_email_lower', func.lower(email), unique=True),
+    )
 
-# Base.metadata.create_all(bind=sync_engine, tables=[User])
+    @validates('email')
+    def validate_email(self, key, value):
+        return value.strip().lower() if value else value
+
+
+Base.metadata.drop_all(sync_engine)
 Base.metadata.create_all(sync_engine)
-# sqlalchemy.create_all(engine)
+
+
+class UserSchema(BaseModel):
+    id: int
+    email: EmailStr
+    name: str
+    is_active: bool
+
+    class Config:
+        from_attributes = True
+
+
 class UserDAL:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def create(self, email, name) -> User:
+    async def create(self, email, name) -> UserSchema:
         user = User(email=email, name=name)
         self.session.add(user)
-        await self.session.flush()
-        return user
+        await self.session.commit()
+        return UserSchema.from_orm(user)
 
-    async def get(self, email):
-        result = await self.session.get(User, {'email': email})
-        return result
+    async def get_by_id(self, user_id: int) -> dict:
+        user = await self.session.get(User, user_id)
+        return UserSchema.model_validate(user).model_dump()
+
+    async def get_by_email(self, email) -> dict:
+        query = (
+            select(User).where(User.email == email)
+        )
+        result = await self.session.execute(query)
+        res = result.all()
+        return UserSchema.model_validate(res).model_dump()
 
     async def all(self) -> List[User]:
         query = select(User)
@@ -75,9 +103,6 @@ class UserDAL:
         print('users!!!!!!')
         print(users)
         return []
-
-
-
 
 
 @app.get('/')
@@ -93,3 +118,16 @@ async def say_hello(name: str):
 @app.post('/user/create')
 async def create_user(email: str, name: str):
     return await UserDAL(session=AsyncSession(engine)).create(email, name)
+
+@app.post('/user/{user_id}')
+async def get_user_by_id(user_id: int):
+    return await UserDAL(session=AsyncSession(engine)).get_by_id(user_id)
+
+@app.post('/user/1/{email}')
+async def get_user_by_email(email: str):
+    return await UserDAL(session=AsyncSession(engine)).get_by_email(email)
+
+
+@app.post('/user/all')
+async def get_all_users():
+    return await UserDAL(session=AsyncSession(engine)).all()
