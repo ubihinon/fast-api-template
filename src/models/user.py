ea71670ba76b087610e0fc8ed1fb3ab06b212010
Vladimir <vladimir@what.digital>
2025-11-30 13:30:19 +0400

    fix

--- src/models/user.py
+++ src/models/user.py
@@ -2,10 +2,12 @@ import datetime
 import uuid
 from typing import Annotated
 
-from sqlalchemy import func, Index, String, text, UUID
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, validates
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy import text, UUID
+from sqlalchemy.orm import DeclarativeBase, mapped_column
 
 from db.session import sync_engine
+from models.mixins import CreatedUpdatedMixin, IdIntPkMixin
 
 
 class Base(DeclarativeBase):
@@ -30,20 +32,24 @@ UpdatedAt = Annotated[
 ]
 
 
-class User(Base):
-    __tablename__ = 'user'
+# class User(Base):
+#     __tablename__ = 'user'
+#
+#     id: Mapped[IntPK]
+#     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+#     name: Mapped[str] = mapped_column(String(100), nullable=False)
+#     is_active: Mapped[bool] = mapped_column(default=False)
+#     created_at: Mapped[CreatedAt]
+#     updated_at: Mapped[UpdatedAt]
+#
+#     __table_args__ = (
+#         Index('ix_user_email_lower', func.lower(email), unique=True),
+#     )
+#
+#     @validates('email')
+#     def validate_email(self, key, value):
+#         return value.strip().lower() if value else value
 
-    id: Mapped[IntPK]
-    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
-    name: Mapped[str] = mapped_column(String(100), nullable=False)
-    is_active: Mapped[bool] = mapped_column(default=False)
-    created_at: Mapped[CreatedAt]
-    updated_at: Mapped[UpdatedAt]
 
-    __table_args__ = (
-        Index('ix_user_email_lower', func.lower(email), unique=True),
-    )
-
-    @validates('email')
-    def validate_email(self, key, value):
-        return value.strip().lower() if value else value
+class User(IdIntPkMixin, CreatedUpdatedMixin, SQLAlchemyBaseUserTable[int], Base):
+    pass
