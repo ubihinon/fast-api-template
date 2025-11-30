a4199696548ac1826f96622f0361da5ef298a21d
Vladimir <vladimir@what.digital>
2025-11-30 21:51:57 +0400

    fix

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -1,11 +1,31 @@
-from pydantic import BaseModel, EmailStr
+import datetime
+
+from fastapi_users import schemas
+
+from api.types.user_id import UserIdType
 
 
-class UserSchema(BaseModel):
-    id: int
-    email: EmailStr
-    name: str
-    is_active: bool
+class UserRead(schemas.BaseUser[UserIdType]):
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
 
-    class Config:
-        from_attributes = True
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
+
+
+# from pydantic import BaseModel, EmailStr
+#
+#
+# class UserSchema(BaseModel):
+#     id: int
+#     email: EmailStr
+#     name: str
+#     is_active: bool
+#
+#     class Config:
+#         from_attributes = True
