34572e58fc38519960e777f6c19995b34b5f9424
ubihinon <ubihinon1@gmail.com>
2025-11-30 21:45:13 +0400

    fix

--- src/api/dependencies/user_manager.py
+++ src/api/dependencies/user_manager.py
@@ -1,2 +1,11 @@
-async def get_user_manager(user_db=Depends(get_user_db)):
+from typing import Annotated
+
+from fastapi import Depends
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+
+from api.auth.user_manager import UserManager
+from api.dependencies.users import get_user_db
+
+
+async def get_user_manager(user_db: Annotated[SQLAlchemyUserDatabase, Depends(get_user_db)]):
     yield UserManager(user_db)
