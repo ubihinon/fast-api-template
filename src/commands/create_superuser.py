0336ccb350a004fa69466146615d65ed7bf5644e
Vladimir <vladimir@what.digital>
2025-12-01 23:52:48 +0400

    fix

---
+++ src/commands/create_superuser.py
@@ -0,0 +1,58 @@
+import asyncio
+import contextlib
+import logging
+
+from fastapi_users.exceptions import UserAlreadyExists
+
+from api.auth.user_manager import UserManager
+from api.dependencies.auth.user_manager import get_user_manager
+from api.dependencies.auth.users import get_user_db
+from db.session import async_session, get_session
+from models import User
+from schemas.user import UserCreate
+
+
+logger = logging.getLogger(__name__)
+
+get_user_db_context = contextlib.asynccontextmanager(get_user_db)
+get_user_manager_context = contextlib.asynccontextmanager(get_user_manager)
+
+
+async def create_user(user_manager: UserManager, user_create: UserCreate) -> User:
+    try:
+        user = await user_manager.create(user_create=user_create, safe=False)
+    except UserAlreadyExists:
+        logger.error(f'User {user_create.email} already exists')
+        raise
+    return user
+
+
+default_email = 'admin@admin.com'
+default_password = '123456'
+default_is_active = True
+default_is_superuser = True
+default_is_verified = True
+
+
+async def create_superuser(
+    email: str = default_email,
+    password: str = default_password,
+    is_active: bool = default_is_active,
+    is_superuser: bool = default_is_superuser,
+    is_verified: bool = default_is_verified
+):
+    user_create = UserCreate(
+        email=email,
+        password=password,
+        is_active=is_active,
+        is_superuser=is_superuser,
+        is_verified=is_verified
+    )
+    async with async_session() as session:
+        async with get_user_db_context(session) as user_db:
+            async with get_user_manager_context(user_db) as user_manager:
+                return await create_user(user_manager, user_create)
+
+
+if __name__ == '__main__':
+    asyncio.run(create_superuser())
