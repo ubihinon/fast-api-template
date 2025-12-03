345f8d31bf440fa6b0714943e6ef1d7ad9df5977
ubihinon <ubihinon1@gmail.com>
2025-12-03 22:00:03 +0400

    fix

--- src/api/v1/auth.py
+++ src/api/v1/auth.py
@@ -15,3 +15,6 @@ router.include_router(
 router.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
 )
+
+# /request-verify-token and /verify
+router.include_router(fastapi_users.get_verify_router(UserRead))
