d20db5a58f6e05c4329abf445dba7b33549ce8c6
ubihinon <ubihinon1@gmail.com>
2025-12-03 22:27:13 +0400

    fix

---
+++ src/api/v1/messages.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+
+from api.dependencies.auth.fastapi_users import current_active_superuser, current_active_user
+from models import User
+from schemas.user import UserRead
+
+router = APIRouter(prefix='/messages', tags=['Messages'])
+
+
+@router.get('')
+async def get_messages(user: Annotated[User, Depends(current_active_user)]):
+    return {
+        'messages': ['m1', 'm2', 'm3'],
+        'user': UserRead.model_validate(user),
+    }
+
+
+@router.get('/secrets')
+async def get_superuser_messages(user: Annotated[User, Depends(current_active_superuser)]):
+    return {
+        'messages': ['secret-m1', 'secret-m2', 'secret-m3'],
+        'user': UserRead.model_validate(user),
+    }
