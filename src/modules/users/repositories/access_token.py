edadfbf79d0944b776f7e5c0222a8510ff4fec9d
ubihinon <ubihinon1@gmail.com>
2026-06-17 22:50:05 +0400

    fix

--- src/modules/users/repositories/access_token.py
+++ src/modules/users/repositories/access_token.py
@@ -1,5 +1,6 @@
 import datetime
 
+from sqlalchemy import update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models.types import UserIdType
@@ -20,3 +21,29 @@ class AccessTokenRepository:
         self.session.add(access_token)
         await self.session.flush()
         return AccessTokenSchema.model_validate(access_token)
+
+    async def deactivate_token(self, user_id: UserIdType, token: str) -> bool:
+        result = await self.session.execute(
+            update(AccessToken)
+            .where(
+                AccessToken.token == token,
+                AccessToken.user_id == user_id
+            )
+            .values(is_active=False)
+        )
+        await self.session.commit()
+
+        return result.rowcount > 0
+
+    async def deactivate_all_tokens(self, user_id: UserIdType) -> bool:
+        result = await self.session.execute(
+            update(AccessToken)
+            .where(
+                AccessToken.user_id == user_id,
+                AccessToken.is_active == True
+            )
+            .values(is_active=False)
+        )
+        await self.session.commit()
+
+        return result.rowcount > 0
