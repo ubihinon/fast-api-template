2cb308b390144a18a0b70ce9b56dcb1c8576f09f
Vladimir <vladimir@what.digital>
2026-06-25 13:18:49 +0400

    fix

--- src/modules/users/manager.py
+++ src/modules/users/manager.py
@@ -4,15 +4,15 @@ from fastapi import Request
 from fastapi_users import BaseUserManager, IntegerIDMixin
 
 from core.models.types import UserIdType
-from core.settings import RESET_PASSWORD_TOKEN_SECRET, VERIFICATION_TOKEN_SECRET
+from core.settings import settings
 from modules.users.models import User
 
 logger = logging.getLogger(__name__)
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, UserIdType]):
-    reset_password_token_secret = RESET_PASSWORD_TOKEN_SECRET
-    verification_token_secret = VERIFICATION_TOKEN_SECRET
+    reset_password_token_secret = settings.RESET_PASSWORD_TOKEN_SECRET
+    verification_token_secret = settings.VERIFICATION_TOKEN_SECRET
 
     async def on_after_register(self, user: User, request: Request | None = None):
         logger.warning("User %s has registered.", user.id)
