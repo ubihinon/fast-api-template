1c00a12db849434a5c537e4e127dfb0910d9b51f
ubihinon <ubihinon1@gmail.com>
2026-07-25 23:43:10 +0400

    fix

--- src/modules/users/dependencies.py
+++ src/modules/users/dependencies.py
@@ -5,6 +5,8 @@ from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.database import get_session
+from modules.notifications.dependencies import get_users_email_service
+from modules.notifications.services.users_email import UsersEmailService
 from modules.users.manager import UserManager
 from modules.users.models import AccessToken, User
 
@@ -13,8 +15,11 @@ async def get_user_db(session: Annotated[AsyncSession, Depends(get_session)]):
     yield User.get_db(session)
 
 
-async def get_user_manager(user_db: Annotated[SQLAlchemyUserDatabase, Depends(get_user_db)]):
-    yield UserManager(user_db)
+async def get_user_manager(
+    user_db: Annotated[SQLAlchemyUserDatabase, Depends(get_user_db)],
+    email_service: Annotated[UsersEmailService, Depends(get_users_email_service)],
+):
+    yield UserManager(user_db, email_service)
 
 
 async def get_access_token_db(session: Annotated[AsyncSession, Depends(get_session)]):
