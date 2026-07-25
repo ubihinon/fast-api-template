937cb635126c1fb5802febff7126c5c6cb593aaf
Vladimir <vladimir@what.digital>
2026-07-26 00:14:38 +0400

    fix

--- src/modules/users/repositories/user.py
+++ src/modules/users/repositories/user.py
@@ -5,7 +5,3 @@ from modules.users.repositories.base import BaseRepository
 class UserRepository(BaseRepository):
     async def get(self, user_id: int) -> User | None:
         return await self.session.get(User, user_id)
-
-    async def get_by_email(self, email: str) -> User | None:
-        user_db = User.get_db(self.session)
-        return await user_db.get_by_email(email)
