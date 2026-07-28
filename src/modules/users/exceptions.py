cfa57ef89a0c129896d1ebdab40eb462156a5113
ubihinon <ubihinon1@gmail.com>
2026-07-28 17:36:28 +0400

    fix

--- src/modules/users/exceptions.py
+++ src/modules/users/exceptions.py
@@ -1,3 +1,6 @@
+from core.i18n import _
+
+
 class AccessTokenNotFound(Exception):
     pass
 
@@ -11,21 +14,21 @@ class AuthErrorException(Exception):
 class UserNotFoundException(Exception):
     def __init__(self, email: str):
         self.email = email
-        super().__init__(f"User with email {email} not found")
+        super().__init__(_("User with email %(email)s not found") % {"email": email})
 
 
 class LoginCodeInvalidException(Exception):
     def __init__(self):
-        super().__init__("Code is invalid or expired")
+        super().__init__(_("Code is invalid or expired"))
 
 
 class LoginCodeNotFoundException(Exception):
     def __init__(self, code_id: int):
         self.code_id = code_id
-        super().__init__(f"Code with id '{code_id}' not found")
+        super().__init__(_("Code with id '%(code_id)s' not found") % {"code_id": code_id})
 
 
 class LoginMaxNumberAttemptsException(Exception):
-    def __init__(self, message: str = "Maximum number of attempts exceeded"):
-        self.message = message
-        super().__init__(message)
+    def __init__(self, message: str = ""):
+        self.message = message or _("Maximum number of attempts exceeded")
+        super().__init__(self.message)
