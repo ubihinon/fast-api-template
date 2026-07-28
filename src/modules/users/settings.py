1ab8126aefe6f77d090bae16ee53163ed709724f
ubihinon <ubihinon1@gmail.com>
2026-07-28 12:09:45 +0400

    fix

--- src/modules/users/settings.py
+++ src/modules/users/settings.py
@@ -1,6 +1,10 @@
 import datetime
 
-ACCESS_TOKEN_EXPIRES_IN_TIMEDELTA: datetime.timedelta = datetime.timedelta(hours=1)
+from core.settings import settings
+
+ACCESS_TOKEN_EXPIRES_IN_TIMEDELTA: datetime.timedelta = datetime.timedelta(
+    seconds=settings.ACCESS_TOKEN_LIFETIME_SECONDS
+)
 LOGIN_CODE_EXPIRES_IN_TIMEDELTA: datetime.timedelta = datetime.timedelta(minutes=15)
 
 MAX_LOGIN_ATTEMPTS = 5
