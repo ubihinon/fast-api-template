d8777e50dc5a976223bb9ff625748c47660900b2
ubihinon <ubihinon1@gmail.com>
2026-07-28 13:46:17 +0400

    fix

--- src/modules/notifications/services/users_email.py
+++ src/modules/notifications/services/users_email.py
@@ -26,7 +26,7 @@ class UsersEmailService(BaseEmailService):
         )
         return await self.send_email_async(payload, template_name="users/welcome.html")
 
-    def send_login_code_email_task(self, email: str, login_code: str, expires_in: datetime.timedelta) -> bool:
+    def send_login_code_email_task(self, email: str, login_code: str, expires_in: datetime.timedelta) -> None:
         payload = EmailPayload(
             recipients=[email],
             subject="Login code",
@@ -34,7 +34,7 @@ class UsersEmailService(BaseEmailService):
         )
         return self.send_email_background(payload, template_name="users/login_code.html")
 
-    def send_welcome_email_task(self, email: str) -> bool:
+    def send_welcome_email_task(self, email: str) -> None:
         payload = EmailPayload(
             recipients=[email],
             subject="Welcome to our service!",
