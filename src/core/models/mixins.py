00e3b045d617d2153b068c853f8c5983bc1967dd
ubihinon <ubihinon1@gmail.com>
2026-07-28 12:05:07 +0400

    fix

--- src/core/models/mixins.py
+++ src/core/models/mixins.py
@@ -13,5 +13,5 @@ class CreatedUpdatedMixin:
         DateTime(timezone=True), server_default=func.now(), nullable=False,
     )
     updated_at: Mapped[datetime.datetime] = mapped_column(
-        DateTime(timezone=True), server_default=func.now(), server_onupdate=func.now(), nullable=False,
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False,
     )
